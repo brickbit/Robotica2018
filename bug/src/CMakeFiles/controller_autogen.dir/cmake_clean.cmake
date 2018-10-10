f394c6023c6ca3b827d4d23efe8f1e561f1ecd72
brickbit <92rober@gmail.com>
2018-10-10 17:16:36 +0200

    bug2

---
+++ bug/src/CMakeFiles/controller_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "controller_autogen"
+  "CMakeFiles/controller_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/controller_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/controller_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
