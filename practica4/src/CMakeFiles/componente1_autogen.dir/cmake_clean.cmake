9f2f04f20173e01cce604b8ab3b78c496d3c9f31
brickbit <92rober@gmail.com>
2019-01-14 18:34:36 +0100

    some fixes

---
+++ practica4/src/CMakeFiles/componente1_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "componente1_autogen"
+  "CMakeFiles/componente1_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/componente1_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/componente1_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
