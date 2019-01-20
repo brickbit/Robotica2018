43ad06ea7c9b199296e2c0675a80f3b210c5719f
brickbit <92rober@gmail.com>
2019-01-20 12:40:12 +0100

    added iteration5 and supervisor almost finished

---
+++ supervisor/src/CMakeFiles/supervisor.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "supervisor_autogen"
+  "CMakeFiles/supervisor_autogen.dir/AutogenOldSettings.cmake"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "GotoPoint.cpp"
+  "GotoPoint.h"
+  "AprilTags.cpp"
+  "AprilTags.h"
+  "GenericBase.cpp"
+  "GenericBase.h"
+  "JointMotor.cpp"
+  "JointMotor.h"
+  "DifferentialRobot.cpp"
+  "DifferentialRobot.h"
+  "ui_mainUI.h"
+  "CMakeFiles/supervisor.dir/specificworker.cpp.o"
+  "CMakeFiles/supervisor.dir/specificmonitor.cpp.o"
+  "CMakeFiles/supervisor.dir/home/brickbit/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/supervisor.dir/home/brickbit/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "CMakeFiles/supervisor.dir/home/brickbit/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/supervisor.dir/main.cpp.o"
+  "CMakeFiles/supervisor.dir/genericmonitor.cpp.o"
+  "CMakeFiles/supervisor.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/supervisor.dir/genericworker.cpp.o"
+  "CMakeFiles/supervisor.dir/apriltagsI.cpp.o"
+  "CMakeFiles/supervisor.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/supervisor.dir/GotoPoint.cpp.o"
+  "CMakeFiles/supervisor.dir/AprilTags.cpp.o"
+  "CMakeFiles/supervisor.dir/GenericBase.cpp.o"
+  "CMakeFiles/supervisor.dir/JointMotor.cpp.o"
+  "CMakeFiles/supervisor.dir/DifferentialRobot.cpp.o"
+  "CMakeFiles/supervisor.dir/supervisor_autogen/mocs_compilation.cpp.o"
+  "../bin/supervisor.pdb"
+  "../bin/supervisor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/supervisor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ supervisor/src/specificworker.cpp
@@ -0,0 +1,114 @@
+/*
+ *    Copyright (C)2019 by YOUR NAME HERE
+ *
+ *    This file is part of RoboComp
+ *
+ *    RoboComp is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    RoboComp is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "specificworker.h"
+
+/**
+* \brief Default constructor
+*/
+SpecificWorker::SpecificWorker(MapPrx& mprx) : GenericWorker(mprx)
+{
+
+}
+
+/**
+* \brief Default destructor
+*/
+SpecificWorker::~SpecificWorker()
+{
+	std::cout << "Destroying SpecificWorker" << std::endl;
+}
+
+bool SpecificWorker::setParams(RoboCompCommonBehavior::ParameterList params)
+{
+    	innerModel= new InnerModel ( "/home/brickbit/robocomp/files/innermodel/informatica-tercio.xml" );
+	tag.model=innerModel;   	
+	timer.start ( Period );
+
+	return true;
+}
+
+void SpecificWorker::compute()
+{
+RoboCompGenericBase::TBaseState bState;
+	differentialrobot_proxy->getBaseState(bState);
+	innerModel->updateTransformValues("base", bState.x,0,bState.z,0 ,bState.alpha,0);
+	
+	switch (estado)
+			{	
+				case State::SEARCH:	
+					
+					qDebug()<<"SEARH";
+					qDebug()<<"Current en el search"<<current;
+					qDebug()<<"tag getId"<<tag.getID();
+					
+					if(tag.getID() == current)
+					{	
+						qDebug()<<"Son iguales tag y current";
+						differentialrobot_proxy->stopBase();
+						
+						gotopoint_proxy->go("", tag.getCurrentPose().x(), tag.getCurrentPose().z(), 0);
+
+						//differentialrobot_proxy->stopBase();
+						//gotopoint_proxy->go("", tag.getCurrentPose().x(), tag.getCurrentPose().z(), 0);
+						qDebug()<<"Llega";
+						estado = State::GO;
+						qDebug()<<"De SEARH a GO";
+					}
+					qDebug()<<"Girando";
+					gotopoint_proxy ->turn(0.6);
+					//differentialrobot_proxy->setSpeedBase(0,0.3);
+					break;
+				case State::GO:
+					qDebug()<<"Entrando en go"<<gotopoint_proxy->atTarget();
+					
+					if( gotopoint_proxy->atTarget() == true)
+					{
+						gotopoint_proxy->stop();
+						//differentialrobot_proxy->stopBase();
+						estado = State::SEARCH;
+						qDebug()<<"De GO  a SEARCH";
+						current = (current+1);
+						qDebug()<<"Current"<<current;
+
+					}
+					/*else if( tag.areEquals() && tag.getID() == current)
+					{
+						
+						gotopoint_proxy->go("", tag.getCurrentPose().x(), tag.getCurrentPose().z(), 0);
+					}*/
+					
+					break;
+			}
+}
+
+
+void SpecificWorker::newAprilTagAndPose(const tagsList &tags, const RoboCompGenericBase::TBaseState &bState, const RoboCompJointMotor::MotorStateMap &hState)
+{
+//subscribesToCODE
+
+}
+
+void SpecificWorker::newAprilTag(const tagsList &tags)
+{
+	qDebug()<<"LLegando las april tags" << tags.front().tx << tags.front().tz;
+	tag.transform(tags.front().tx, tags.front().tz, tags.front().id);
+
+}
+
+
